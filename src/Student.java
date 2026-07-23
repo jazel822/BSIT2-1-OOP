69add2f24bc4ba843194ec657aea7076776ddf3b
jdee80322@liceo.edu.ph <jdee80322@liceo.edu.ph>
2026-07-23 09:32:30 +0800

    prelim-lab-exam

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+public class Student {
+    private String studentId;
+    private String fullName;
+    private String program;
+    private int yearLevel;
 
-public class Class {
+    public Student(String studentId, String fullName, String program, int yearLevel) {
+        this.studentId = studentId;
+        this.fullName = fullName;
+        this.program = program;
+        this.yearLevel = yearLevel;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public int getYearLevel() {
+        return yearLevel;
+    }
+
+    public String describe() {
+        return studentId + " | " + fullName + " | " + program + " | Year " + yearLevel;
+    }
 }
